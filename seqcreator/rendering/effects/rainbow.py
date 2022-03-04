03a21d4496048f2432a7fdc0d6bf28dfbef55c25
Sapir Caduri <sapir.caduri@gmail.com>
2022-03-04 22:20:19 +0200

    adding rainbow effect and linear function

---
+++ seqcreator/rendering/effects/rainbow.py
@@ -0,0 +1,18 @@
+from seqcreator.api import timing
+from seqcreator.api.color import Color
+from seqcreator.rendering.effects.base_effect import BaseEffect
+
+class Rainbow(BaseEffect):
+
+    def __init__(self, start, end):
+        super().__init__()
+        self.function_start = start
+        self.function_end = end
+
+    def get_effect_params_json(self):
+        return {
+        "rainbow": {
+            "hue_start": self.function_start,
+            "hue_end": self.function_end
+            }
+        }
