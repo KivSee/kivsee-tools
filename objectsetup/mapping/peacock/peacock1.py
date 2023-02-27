e9b77a893aa5a8e24784deb99b2a0b521e400f01
Amir Blum <amirgiraffe@gmail.com>
2023-02-27 22:13:20 +0200

    feat: WIP mappings

--- objectsetup/mapping/peacock/peacock1.py
+++ objectsetup/mapping/peacock/peacock1.py
@@ -1,24 +1,94 @@
+import random
+
+wing_l = list(range(0, 115))
+wing_l_r = wing_l.copy()
+random.shuffle(wing_l_r)
+
+def swap(l, i1, i2):
+    t = l[i1]
+    l[i1] = l[i2]
+    l[i2] = t
+
+wing_l_a1 = []
+for i in range(0, len(wing_l), 10):
+    if i < len(wing_l):
+        wing_l_a1.append(i)
+    if i + 1 < len(wing_l):
+        wing_l_a1.append(i + 1)
+    if i + 2 < len(wing_l):
+        wing_l_a1.append(i + 2)
+    if i + 3 < len(wing_l):
+        wing_l_a1.append(i + 3)
+    if i + 4 < len(wing_l):
+        wing_l_a1.append(i + 4)
+
+wing_l_a2 = []
+for i in range(0, len(wing_l), 10):
+    if i + 5 < len(wing_l):
+        wing_l_a2.append(i + 5)
+    if i + 6 < len(wing_l):
+        wing_l_a2.append(i + 6)
+    if i + 7 < len(wing_l):
+        wing_l_a2.append(i + 7)
+    if i + 8 < len(wing_l):
+        wing_l_a2.append(i + 8)
+    if i + 9 < len(wing_l):
+        wing_l_a2.append(i + 9)
+
+wing_l_d = wing_l[0::5]
+
+wing_l_s = []
+for i in range(0, int(len(wing_l) / 2)):
+    wing_l_s.append(i)
+    wing_l_s.append(len(wing_l) - 1 - i)
+if len(wing_l) % 2 == 1:
+    wing_l_s.append(int(len(wing_l) / 2))
+
 val = {
-        "numberOfPixels": 60,
+        "numberOfPixels": 740,
         "segments": [{
-            "name": "all",
-            "pixels": [dict({"index": n}) for n in range(0, 60)]
-        },
-        {
+        #     "name": "all",
+        #     "pixels": [dict({"index": n}) for n in range(0, 720)]
+        # },
+        # {
             "name": "head",
-            "pixels": [dict({"index": n}) for n in range(0, 10)]
+            "pixels": [dict({"index": n}) for n in range(158, 295)]
         },
         {
             "name": "body",
-            "pixels": [dict({"index": n}) for n in range(10, 20)]
+            "pixels": [dict({"index": n}) for n in list(range(305, 385)) + list(range(550, 650))]
         },
         {
             "name": "wing_l",
-            "pixels": [dict({"index": n}) for n in range(20, 30)]
+            "pixels": [dict({"index": n}) for n in range(0, 115)]
         },
         {
-            "name": "wing_r",
-            "pixels": [dict({"index": n}) for n in range(30, 40)]
-        }
+            "name": "wing_l_r",
+            "pixels": [dict({"index": n}) for n in wing_l_r]
+        },
+        {
+            "name": "wing_l_a1",
+            "pixels": [dict({"index": n}) for n in wing_l_a1]
+        },
+        {
+            "name": "wing_l_a2",
+            "pixels": [dict({"index": n}) for n in wing_l_a2]
+        },
+        {
+            "name": "wing_l_d",
+            "pixels": [dict({"index": n}) for n in wing_l_d]
+        },
+        {
+            "name": "wing_l_s",
+            "pixels": [dict({"index": n}) for n in wing_l_s]
+        },
+        {
+            "name": "tail",
+            "pixels": [dict({"index": n}) for n in range(400, 530)]
+        },
+        # {
+        #     "name": "wing_r",
+        #     "pixels": [dict({"index": n}) for n in range(0, 0)]
+        # }
         ]
     }
--- seqcreator/users/common/songs/nyan.py
+++ seqcreator/users/common/songs/nyan.py
@@ -5,6 +5,17 @@ from seqcreator.rendering.functions.functions_store import const_function
 
 import random
 
+wingl = ("peacock1", "wing_l")
+wingl_r = ("peacock1", "wing_l_r")
+wingl_a1 = ("peacock1", "wing_l_a1")
+wingl_a2 = ("peacock1", "wing_l_a2")
+wingl_d = ("peacock1", "wing_l_d")
+wingl_s = ("peacock1", "wing_l_s")
+body = ("peacock1", "body")
+head = ("peacock1", "head")
+tail = ("peacock1", "tail")
+all = [wingl, body, head, tail]
+
 class Nyan(Song):
     def __init__(self):
         super().__init__("nyan", 2.45*60*1000, 0)
@@ -13,17 +24,38 @@ class Nyan(Song):
     def render_effects(self):
         timing.song_settings(bpm=128, beats_per_episode=8, start_offset=0)
 
-        self.elements.set(self.elements.all())
-        # self.elements.set(self.elements.all_odd())
-
-        for ep in range(1, 9):
+        for ep in range(1, 17*3):
             timing.episode(ep)
-            self.set_random_coloring()
+            self.elements.set([wingl])
+            self.rainbow_color()
+
+            self.elements.set([wingl_a1])
+            timing.cycle(1)
+            masking.brightness_saw(0.3)
+            self.elements.set([wingl_a2])
+            timing.cycle(4)
+            masking.brightness_saw(0)
+
+        # for ep in range(1, 17*3):
+        #     timing.episode(ep)
+
+        #     self.elements.set([wingl_a1])
+        #     self.rainbow_color()
+        #     self.elements.set([wingl_a2])
+        #     self.white()
 
         # all first section
-        timing.episodes(1, 9)
-        timing.cycle(1)
-        masking.snake()
+        # timing.episodes(1, 17*3)
+
+        # timing.cycle(4)
+        # self.elements.set([wingl_s])
+        # masking.snake()
+        # masking.brightness_saw(0)
+
+        # timing.cycle(1)
+        # self.elements.set([wingl_a1])
+        # masking.brightness_saw(0.5)
+
 
         return
 
@@ -74,23 +106,31 @@ class Nyan(Song):
         # timing.beats(0, 130)    
         # masking.brightness(0.8)
 
+    def white(self):
+        coloring.uniform(color.Color(random.uniform(0.0, 1.0), 0.0, 0.7))
+
     def uniform_color(self):
-        self.elements.set(self.elements.all())
         coloring.uniform(color.Color(random.uniform(0.0, 1.0), random.uniform(0.8, 1.0), 1.0))
 
     def rainbow_color(self):
-        self.elements.set(self.elements.all())
         start = random.uniform(0.0, 1.0)
         coloring.rainbow_static(start, start + 1.0)
 
+    def grad(self):
+        start = random.uniform(0.0, 1.0)
+        coloring.rainbow_static(start, start + 0.5)
+
     def segments_color(self):
-        self.elements.set([("peacock1", "head")])
+        # self.elements.set([("peacock1", "head")])
         coloring.uniform(color.Color(random.uniform(0.0, 1.0), random.uniform(0.8, 1.0), 1.0))
 
     # same color different sat for each segment
-    # 2 colors on segments
+    # 2 colors on segments from list of good looking options
+    # full rainbow
+    # small range rainbow
+    # 
 
     def set_random_coloring(self):
-        coloring_func = random.choice([self.segments_color])
-        # coloring_func = random.choice([self.uniform_color, self.rainbow_color, self.segments_color])
+        coloring_func = random.choice([self.grad])
+        # coloring_func = random.choice([self.uniform_color, self.rainbow_color, self.segments_color, self.grad])
         coloring_func()
